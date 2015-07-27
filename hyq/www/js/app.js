17a4a9a2e925557f03e06d28281d22595b910d4c
bao hao <905049286@qq.com>
2015-07-27 19:27:53 +0800

    app配置

--- hyq/www/js/app.js
+++ hyq/www/js/app.js
@@ -61,7 +61,9 @@ angular.module('hyq', [
     $ionicConfigProvider.navBar.alignTitle('center');               // 标题的位置
     $ionicConfigProvider.navBar.positionPrimaryButtons('left');     // 主要操作按钮的位置
     $ionicConfigProvider.navBar.positionSecondaryButtons('right');  // 次要操作按钮的位置
-    $ionicConfigProvider.views.maxCache(0);
+    $ionicConfigProvider.form.toggle('large');
+    $ionicConfigProvider.spinner.icon('ios');
+    //$ionicConfigProvider.views.maxCache(0);
 
     $stateProvider
 
